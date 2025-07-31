a539caf9a31897ac4e52f3e6bd397c46697d91e5
Sandra Gonzalez Iglesias <sgonzalez@veryfica.com>
2025-07-31 12:30:21 +0200

    Add metodo get

--- SRC/index.js
+++ SRC/index.js
@@ -36,4 +36,45 @@ const getConnection = async () => {
 };
 
 
-// Endpoint para obtener todas las tareas
+// Endpoint para obtener todas las tareas. CRUD
+
+// GET /frases - Listar todas las frases (con información del personaje y el título del capítulo)
+
+
+server.get("/frases", async (req, res) => {
+    // recojo los datos que me envía el frontend y los guardo en una constante
+    const nombre_personaje = req.query.nombre_personaje;
+    console.log('nombre_personaje:', nombre_personaje);
+
+    // Conecto con la base de datos
+
+    const connection = await getConnection();
+
+    // Realizo la query a la base de datos
+    const QuerySQL = await connection.execute("SELECT * FROM frases WHERE nombre_personaje = ?", [nombre_personaje]);
+    const [result] = await connection.query(QuerySQL, [nombre_personaje]);
+    console.log("result es:", result);
+
+    res.json(result);
+
+    // Cierro la conexión
+    await connection.end();
+    // Envío la respuesta al frontend
+     if (result.length === 0) {
+    res.status(404).json({
+      status: "error",
+      message: "No se encuentra tu solicitud",
+    });
+  } else {
+    res.status(201).json({
+      status: "succes",
+      message: result,
+    });
+  }
+});
+
+//post: enviar datos al servidor, insertar una frase nueva
+
+//delete: eliminar datos
+
+
